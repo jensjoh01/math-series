398570f6df9e46d7b261400ba7569b28d251b97a
Joseph Kim <joseph.kim.kr@gmail.com>
2017-10-17 14:41:22 -0700

    wrote tests for lucas function

--- test_series.py
+++ test_series.py
@@ -1,15 +1,28 @@
 """This is the test file for module series.py."""
 import pytest
 
-# (n, result)
-n_values = [
+# for testing fibonacci
+# pairs: (n, expected_result)
+fib_n_values = [
     (0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 5), (6, 8), (13, 233),
     (20, 6765), (30, 832040)
 ]
 
+lucas_n_values = [
+    (0, 2), (1, 1), (2, 3), (3, 4), (4, 7), (5, 11), (6, 18), (13, 521),
+    (20, 15127), (30, 1860498)
+]
 
-@pytest.mark.parametrize('n, result', n_values)
+
+@pytest.mark.parametrize('n, result', fib_n_values)
 def test_fib(n, result):
     """Test for fib using n_values list."""
     from series import fib
     assert fib(n) == result
+
+
+@pytest.mark.parametrize('n, result', lucas_n_values)
+def test_lucas(n, result):
+    """Test for lucas series using n_values list."""
+    from series import lucas
+    assert lucas(n) == result
